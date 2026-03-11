430d995d61e3327a201faf72048cc52e7c1ddef7
Sasaki Haruna <sasaki.haruna@flymee.co.jp>
2026-03-11 12:40:02 +0900

    fix: reactプラグインの型を明示的に指定

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     // React の JSX 構文を解釈できるようにプラグインを追加
-    plugins: [react()],
+    plugins: [react() as any],
     resolve: {
         // pnpm 環境で複数の React が読み込まれるケースを防ぐ
         dedupe: ["react", "react-dom"],
